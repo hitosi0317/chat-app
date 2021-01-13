2a8f6bc18e899988050068ef5f11c55cb319b383
hitosi0317 <kondahitosi@gmail.com>
2021-01-14 03:54:40 +0900

    チャットルーム作成機能の実装

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,4 +1,6 @@
 class RoomUser < ApplicationRecord
   belongs_to :room
   belongs_to :user #どのユーザーがどのチャットルームに参加しているかを管理するのが、中間テーブルである「room_userテーブル」となります。
+
+  validates :name, presence: true #チャットルームを新規作成するにあたって「ルーム名」は必ず必要なので、上記のバリデーションは「ルーム名が存在（presence）している場合のみ作成可（true）」という意味です。
 end

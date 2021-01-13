bdaf793f5bad33e2e3266da078fc601dd7b62d7a
hitosi0317 <kondahitosi@gmail.com>
2021-01-14 00:17:24 +0900

    room_usersテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
+  has_many :rooms_users #1つのチャットルームには、2人のユーザーが存在します。
+  has_many :users, through: :rooms_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+has_many :rooms_users #1人のユーザーは複数のチャットルームに参加出来ます。
+has_many :rooms, through: :room_users
 end

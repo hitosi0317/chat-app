0a0242c47e8788b18d60276c1c4baae4f2ad9530
hitosi0317 <kondahitosi@gmail.com>
2021-01-14 01:33:58 +0900

    プルダウンの実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,4 @@
 class Room < ApplicationRecord
-  has_many :rooms_users #1つのチャットルームには、2人のユーザーが存在します。
-  has_many :users, through: :rooms_users
+  has_many :room_users #1つのチャットルームには、2人のユーザーが存在します。
+  has_many :users, through: :room_users
 end
